4cd327552c5c7575628463541417828d9e9a1112
Joseph Hangarter <47258807+JCode1986@users.noreply.github.com>
2020-01-28 20:38:44 -0800

    header and footer completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Odometer from "./components/Odometer/Odometer";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Footer = () => <footer>&copy; 2019 Code Fellows</footer>;
+
+export default Footer;
